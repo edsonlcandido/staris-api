b9c63335c90094cc4d93c849ce1ff93f8610f73f
edsonlcandido <38674440+edsonlcandido@users.noreply.github.com>
2024-05-03 03:25:24 +0000

    criado banco de dados SQLite app.db

--- src/Starwars.Api/Program.cs
+++ src/Starwars.Api/Program.cs
@@ -1,7 +1,18 @@
 using EvolveDb;
+using Microsoft.Data.Sqlite;
+
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+var outputPath = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory());
+var cnx = new SqliteConnection($@"Data Source={System.IO.Path.Combine(outputPath,"app.db")}");
+var evolve = new Evolve(cnx)
+{
+    Locations = new List<string> { "Data/Migrations" },
+    IsEraseDisabled = true,
+};
+
+evolve.Migrate();
 
 app.MapGet("/", () => "Hello World!");
 
