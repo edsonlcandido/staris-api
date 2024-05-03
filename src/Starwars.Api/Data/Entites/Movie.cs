579fee193db0b33814dbf0625aa903968eda4f21
edsonlcandido <38674440+edsonlcandido@users.noreply.github.com>
2024-05-03 03:15:13 +0000

    initial commit

---
+++ src/Starwars.Api/Data/Entites/Movie.cs
@@ -0,0 +1,9 @@
+public class Movie{
+    public int Id { get; set; }    
+    public string Title { get; set; }
+    public string Episode { get; set; }
+    public string OpeningCrawl { get; set; }
+    public string Director { get; set; }
+    public string Producer { get; set; }
+    public string ReleaseDate { get; set; }
+}
---
+++ src/Starwars.Api/Data/Migrations/V2024_05_02__Create_table_movies.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Movies (
+    Id INTEGER PRIMARY KEY,
+    Title TEXT,
+    Episode INTEGER,
+    Director TEXT,
+    Producer TEXT,
+    ReleaseDate DATE
+);
